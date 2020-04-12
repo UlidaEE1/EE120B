420f039a00f34b5996576fb1a0281f8d36595068
ulises lazaro quiroz <ulaza001@wch136-09.cs.ucr.edu>
2020-04-12 15:08:44 -0700

    p2

--- lab2/turnin/ulaza001_lab2_part1.c
+++ lab2/turnin/ulaza001_lab2_part1.c
@@ -14,15 +14,35 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
-     DDRA = 0x00; PORTA = 0xFF ; // INPUT
+    	 DDRA = 0x00; PORTA = 0xFF ; // INPUT
 	DDRB = 0xFF ; PORTB = 0x00; // OUTPUT
+	unsigned char tempA = 0x00; // temporaery 
 	unsigned char tempB = 0x00;  // variable for bit manipulation 
 
     /* Insert your solution below */
     while (1) {
-	tempB = 0x00;
-	tempB = ~PINA;
+	tempA = PINA;
+
+	if( tempA ==  0x00)
+		{
+			tempB = 0x00;
+		}
+	else if( tempA ==  0x01)
+                {
+                        tempB = 0x01;
+                }
+	else if( tempA ==  0x02)
+                {
+                        tempB = 0x00;
+                }
+	else if( tempA ==  0x03)
+                {
+                        tempB = 0x00;
+                }
 	PORTB = tempB;
+
+
+
     }
     return 1;
 }
---
+++ lab2p2/source/main.c
@@ -0,0 +1,47 @@
+/*      Author: ulaza001
+ *       *  Partner(s) Name:
+ *        *      Lab Section:
+ *         *      Assignment: Lab #  Exercise #
+ *          *      Exercise Description: [optional - include for your own benefit]
+ *           *
+ *            *      I acknowledge all content contained herein, excluding template or example
+ *             *      code, is my own original work.
+ #include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+         DDRA = 0x00; PORTA = 0xFF ; // INPUT
+        DDRB = 0xFF ; PORTB = 0x00; // OUTPUT
+        unsigned char tempA = 0x00; // temporaery
+        unsigned char tempB = 0x00;  // variable for bit manipulation
+
+    /* Insert your solution below */
+    while (1) {
+        tempA = PINA;
+
+        if( tempA ==  0x00)
+                {
+                        tempB = 0x00;
+                }
+        else if( tempA ==  0x01)
+                {
+                        tempB = 0x01;
+                }
+        else if( tempA ==  0x02)
+                {
+                        tempB = 0x00;
+                }
+        else if( tempA ==  0x03)
+                {
+                        tempB = 0x00;
+                }
+        PORTB = tempB;
+
+
+
+    }
+    return 1;
+

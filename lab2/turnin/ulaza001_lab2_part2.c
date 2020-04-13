bcfe748c3c6c47f163da79bc8e8a1f60a58f63bf
ulises lazaro quiroz <ulaza001@wch136-09.cs.ucr.edu>
2020-04-13 10:03:44 -0700

    l2p2

---
+++ lab2/turnin/ulaza001_lab2_part2.c
@@ -0,0 +1,40 @@
+/*	Author: ulaza001
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    	 DDRA = 0x00; PORTA = 0xFF ; // INPUT
+	DDRC = 0xFF ; PORTC = 0x00; // OUTPUT
+	unsigned char tempA = 0x00; // temporaery
+	unsigned char tempB = 0x00; 
+	unsigned char cnt = 0x00;  // variable for bit manipulation 
+	unsigned char i = 0x00;
+    /* Insert your solution below */
+    while (1) {
+	tempA = PINA & 0x0F;
+
+	for ( i = 0x00 ; i < 0x04; i= i + 0x01)
+		{
+		tempB  = tempA & 0x01;
+			if( tempB == 0x00) 
+				{ cnt =( cnt + 0x01);}
+		tempA =( tempA >> 0x01);
+		
+		
+			 }	
+		PORTC = cnt;
+		cnt = 0;
+    }
+    return 0;
+}

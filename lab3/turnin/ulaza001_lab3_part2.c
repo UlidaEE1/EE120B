4f7fea88f81f42832b6771180afc8aa09d7e7bbb
ulises lazaro quiroz <ulaza001@wch136-09.cs.ucr.edu>
2020-04-14 19:32:50 -0700

    p3

---
+++ lab3/turnin/ulaza001_lab3_part2.c
@@ -0,0 +1,50 @@
+/*	Author: ulaza001
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+DDRA = 0x00;  PORTA = 0xFF;
+DDRB = 0x00;  PORTB = 0xFF;
+DDRC = 0xFF;  PORTC = 0x00;
+//unsigned char cnt = 0x00;
+unsigned char tempA = 0x00;
+unsigned char tempB = 0x00;
+//unsigned char tempC = 0x00;
+//unsigned char i = 0x00;
+    /* Insert your solution below */
+    while (1) {
+
+        tempA = PINA;
+	
+ 	if( tempA >= 1 &&  tempA <= 2)
+		{ tempB = 0x20;}	
+	else if( tempA >= 3 &&  tempA <= 4 )
+                { tempB = 0x30;}  
+        else if( tempA >= 5 &&  tempA <= 6 )
+                { tempB = 0x38;}
+	else if( tempA >= 7 &&  tempA <= 9 )
+                { tempB = 0x3A ;}
+	else if( tempA >= 10&&  tempA <= 12)
+                { tempB = 0x3E;}
+	else if( tempA >= 13 &&  tempA <= 15 )
+                { tempB = 0x3F;}
+	if( tempA <= 4 )
+                { tempB = tempB | 0x40;}
+               
+		PORTC = tempB;
+                tempB = 0;
+
+    }
+    return 1;
+}

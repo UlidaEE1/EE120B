fed23c2228d7dee9f319ef785d6deb4299a85e53
ulises lazaro quiroz <ulaza001@wch136-09.cs.ucr.edu>
2020-04-19 17:26:25 -0700

    p3

--- lab4/source/main.c
+++ lab4/source/main.c
@@ -13,52 +13,39 @@
 #endif
 
 
-enum States{ Start, s0, s1, s2, s3} state;
+enum States{ Start, s0, s1, s2, s3, s4}state;
 
 
 void Tick()
 {
 unsigned char A;
 unsigned char cnt;
-A = PINA & 0x03;
+A = PINA ;
 switch(state)
 {
 case Start:
 state = s0;
-PORTC = 7;
 break;
 
 case s0:
-if (A== 0x00)
-{ state = s0;
-PORTC = 0x07;}
-else if(A == 0x01)
-{ state = s1;
-PORTC = 0x07 ; }
-else if(A == 0x02 )
-{state = s2;}
-else if(A == 0x03)
-{ state = s3;}
+if (A == 0x80)
+{ state = s1;}
+else{ state = s0;}
 break;
 
 case s1:
-if(A == 0x01)
-{ state = s1;}
-else if(A == 0x02)
-{state = s2;}
-else if (A == 0x03)
-{ state = s3;}
+if(A == 0x04)
+{ state = s2;}
+else {state = s1;}
 break;
 
 case s2:
-if(A == 0x01)
-{ state = s1;}
+if(A == 0x04)
+{ state = s2;}
 else if(A == 0x02)
-{state = s2;}
-else if( A == 0x03)
-{ state = s3;}
-else if ( A == 0x00)
-{ state = s0;}
+{state = s3;}
+else{ state = s0;}
+{ state = s1;}
 break;
 
 case s3:

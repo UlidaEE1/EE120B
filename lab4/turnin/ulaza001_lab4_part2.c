3d2a3d2518e7a74e912e6729780134bd8b1b085c
UlidaEE1 <43683721+UlidaEE1@users.noreply.github.com>
2020-04-26 21:07:11 -0700

    Update ulaza001_lab4_part2.c

--- lab4/turnin/ulaza001_lab4_part2.c
+++ lab4/turnin/ulaza001_lab4_part2.c
@@ -7,119 +7,122 @@
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
-#include <avr/io.h>
-#ifdef _SIMULATE_
-#include "simAVRHeader.h"
-#endif
 
 
-enum States{ Start, s0, s1, s2, s3} state;
+ #include <avr/io.h>
+ #ifdef _SIMULATE_
+ #include "simAVRHeader.h"
+ #endif
 
 
-void Tick()
-{
-unsigned char A;
-unsigned char cnt;
-A = PINA & 0x03;
-switch(state)
-{
-case Start:
-state = s0;
-PORTC = 7;
-break;
-
-case s0:
-if (A== 0x00)
-{ state = s0;
-PORTC = 0x07;}
-else if(A == 0x01)
-{ state = s1;
-PORTC = 0x07 ; }
-else if(A == 0x02 )
-{state = s2;}
-else if(A == 0x03)
-{ state = s3;}
-break;
-
-case s1:
-if(A == 0x01)
-{ state = s1;}
-else if(A == 0x02)
-{state = s2;}
-else if (A == 0x03)
-{ state = s3;}
-break;
-
-case s2:
-if(A == 0x01)
-{ state = s1;}
-else if(A == 0x02)
-{state = s2;}
-else if( A == 0x03)
-{ state = s3;}
-else if ( A == 0x00)
-{ state = s0;}
-break;
-
-case s3:
-if(A == 0x01)
-{ state = s1;}
-else if(A == 0x02)
-{state = s2;}
-else if( A == 0x03)
-{ state = s3;}
-break;
-
-default:
-state = Start; 
-break;
+ enum States{ Start, s0, s1, s2, s3} state;
 
 
+ void Tick()
+ {
+ unsigned char A;
+ unsigned char cnt;
+ A = ~PINA & 0x03;
+ switch(state)
+ {
+ case Start:
+ state = s0;
+ PORTC = 0;
+ break;
 
-}// transitions
+ case s0:
+ if (A== 0x00)
+ { state = s0;
+ PORTC = 0x00;}
+ else if(A == 0x01)
+ { state = s1;
+ PORTC = 0x00 ; }
+ else if(A == 0x02 )
+ {state = s2;}
+ else if(A == 0x03)
+ { state = s3;}
+ break;
 
-switch(state) {
+ case s1:
+ if(A == 0x01)
+ { state = s1;}
+ else if(A == 0x02)
+ {state = s2;}
+ else if (A == 0x03)
+ { state = s3;}
+ break;
 
-case s0:
-PORTC = 7;
-break;
+ case s2:
+ if(A == 0x01)
+ { state = s1;}
+ else if(A == 0x02)
+ {state = s2;}
+ else if( A == 0x03)
+ { state = s3;}
+ else if ( A == 0x00)
+ { state = s0;}
+ break;
 
-case s1:
-if(PORTC < 9)
-{PORTC = PORTC + 1;}
-break;
+ case s3:
+ if(A == 0x01)
+ { state = s1;}
+ else if(A == 0x02)
+ {state = s2;}
+ else if( A == 0x03)
+ { state = s3;}
+ break;
 
-case s2:
-if( PORTC > 0)
-{
-PORTC = --PORTC ;
-}
-break;
-
-case s3:
-PORTC = 0x00;
-break;
-
-default:
-break;
-}// state actions 
-
-
-}
+ default:
+ state = Start;
+ break;
 
 
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-DDRA = 0x00;  PORTA = 0xFF;
-DDRC = 0x0F;  PORTC = 0x00 ;
-state = Start;
-PORTC = 0; 
-//unsigned char A0 = PINA;
-    /* Insert your solution below */
-    while (1) {
+ }// transitions
 
-Tick();
-    }
-   return 0;
-}
+ switch(state) {
+
+ case s0:
+ PORTC = 0;
+ break;
+
+ case s1:
+ if(PORTC < 9)
+ {PORTC = PORTC + 1;}
+ break;
+
+ case s2:
+ if( PORTC > 0)
+ {
+ PORTC = --PORTC ;
+ }
+ break;
+
+ case s3:
+ PORTC = 0x00;
+ break;
+
+ default:
+ break;
+ }// state actions
+
+
+ }
+
+
+
+ int main(void) {
+ /* Insert DDR and PORT initializations */
+ DDRA = 0x00;  PORTA = 0xFF;
+ DDRC = 0xFF;  PORTC = 0x00 ;
+ state = Start;
+ PORTC = 0;
+ //unsigned char A0 = PINA;
+ /* Insert your solution below */
+ while (1) {
+
+ Tick();
+ }
+ return 0;
+ }
+
